8b8aa45dc9aa59978f13bc06bfe28b4c1ce07646
rpalominosc <r.palominos.c@gmail.com>
2020-03-26 18:52:29 -0300

    Nuevos archivos

---
+++ graficas/Ejemploentry.py
@@ -0,0 +1,2 @@
+from tkinter import *
+raiz=Tk()
---
+++ graficas/practica_label.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root=Tk()
+miframe=Frame(root,width=500,height=400)
+miframe.pack()
+milabel=Label(miframe,text="Pruebas de Label",fg="red",font=("Comic Sans",18))
+milabel.place(x=100,y=200)
+
+
+root.mainloop()
