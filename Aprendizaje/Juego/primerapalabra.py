24f6b3cc2d879b7098cd362b10388dca34342b74
rpalominosc <r.palominos.c@gmail.com>
2020-04-12 14:03:31 -0400

    actualizacion

---
+++ Aprendizaje/Juego/primerapalabra.py
@@ -0,0 +1,10 @@
+import re
+def first_word(texto):
+
+
+texto.strip(None)
+
+print(texto)
+
+
+fi
--- Aprendizaje/Juego/zurdos.py
+++ Aprendizaje/Juego/zurdos.py
@@ -7,13 +7,9 @@ def reviyrempl(lista):
     for elemento in arreglo:
         if re.findall('right', elemento):
             elemento=elemento.replace("right","left")
-
         if elemento == arreglo[-1] or len(arreglo)==1:
             cadena=cadena+elemento
         else:
             cadena=cadena+elemento+","
-
-
-
-    print(cadena)
+return(cadena)
 reviyrempl(("enough", "jokes"))
