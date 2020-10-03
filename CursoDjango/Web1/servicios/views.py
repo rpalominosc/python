683584e715ef50a2bbf6cd436cfe1b2cf4bbd975
rpalominosc <r.palominos.c@gmail.com>
2020-10-03 16:32:08 -0300

    Cambios en los tempates y urls a aplicacion

--- CursoDjango/Web1/servicios/views.py
+++ CursoDjango/Web1/servicios/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from servicios.models import Servicio
 # Create your views here.
+
+
+
+
+def  servicios(request):
+        servicios=Servicio.objects.all()
+        return render(request, "servicios/servicios.html",{"servicios": servicios})
